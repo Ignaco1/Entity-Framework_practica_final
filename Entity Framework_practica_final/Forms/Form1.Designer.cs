f8d4e0145c6728e8eb4d0345ec5aca7a692508c5
Ignacio Carignano <nachocarignano@gmail.com>
2024-09-24 19:21:33 -0300

    Agregar archivos de proyecto.

---
+++ Entity Framework_practica_final/Forms/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Entity_Framework_practica_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_mod = new System.Windows.Forms.Button();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.fecha_picker = new System.Windows.Forms.DateTimePicker();
+            this.txt_edad = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_cerrar);
+            this.groupBox1.Controls.Add(this.btn_eliminar);
+            this.groupBox1.Controls.Add(this.btn_mod);
+            this.groupBox1.Controls.Add(this.btn_agregar);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(720, 465);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vista de datos";
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.Location = new System.Drawing.Point(620, 29);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(94, 23);
+            this.btn_cerrar.TabIndex = 4;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = true;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(244, 29);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(94, 23);
+            this.btn_eliminar.TabIndex = 3;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_mod
+            // 
+            this.btn_mod.Location = new System.Drawing.Point(125, 29);
+            this.btn_mod.Name = "btn_mod";
+            this.btn_mod.Size = new System.Drawing.Size(94, 23);
+            this.btn_mod.TabIndex = 2;
+            this.btn_mod.Text = "Modificar";
+            this.btn_mod.UseVisualStyleBackColor = true;
+            this.btn_mod.Click += new System.EventHandler(this.btn_mod_Click);
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(6, 29);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(94, 23);
+            this.btn_agregar.TabIndex = 1;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 77);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(708, 382);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_cancelar);
+            this.groupBox2.Controls.Add(this.btn_guardar);
+            this.groupBox2.Controls.Add(this.fecha_picker);
+            this.groupBox2.Controls.Add(this.txt_edad);
+            this.groupBox2.Controls.Add(this.txt_nombre);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(739, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(387, 465);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Carga de datos";
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(272, 204);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(94, 23);
+            this.btn_cancelar.TabIndex = 9;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            this.btn_cancelar.Click += new System.EventHandler(this.btn_cancelar_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Location = new System.Drawing.Point(17, 204);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(94, 23);
+            this.btn_guardar.TabIndex = 8;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // fecha_picker
+            // 
+            this.fecha_picker.Location = new System.Drawing.Point(149, 98);
+            this.fecha_picker.Name = "fecha_picker";
+            this.fecha_picker.Size = new System.Drawing.Size(200, 20);
+            this.fecha_picker.TabIndex = 7;
+            // 
+            // txt_edad
+            // 
+            this.txt_edad.Location = new System.Drawing.Point(149, 134);
+            this.txt_edad.Name = "txt_edad";
+            this.txt_edad.Size = new System.Drawing.Size(100, 20);
+            this.txt_edad.TabIndex = 6;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(149, 27);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(145, 20);
+            this.txt_nombre.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha de nacimiento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Edad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 485);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_mod;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.DateTimePicker fecha_picker;
+        private System.Windows.Forms.TextBox txt_edad;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Entity Framework_practica_final/Forms/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entity_Framework_practica_final.Models;
+
+namespace Entity_Framework_practica_final
+{
+    public partial class Form1 : Form
+    {
+        string vari;
+        public Form1()
+        {
+            InitializeComponent();
+
+            ModoVista();
+            txt_edad.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaDatos();
+        }
+
+        private void ModoCarga()
+        {
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = true;
+        }
+
+        private void Limpiar()
+        {
+            txt_nombre.Text = "";
+            txt_edad.Text = "";
+            fecha_picker.Text = "";
+        }
+
+        private int GetId()
+        {
+            try
+            {
+                return Convert.ToInt32(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error a la hora de obtener el id: " + ex.Message);
+            }
+        }
+
+        private Personas GetPersona()
+        {
+            using(DB_PRACTICAEntities db = new DB_PRACTICAEntities())
+            {
+                int id = GetId();
+
+                Personas persona = db.Personas.Find(id);
+
+                return persona;
+            }
+        }
+
+        private void ModoVista()
+        {
+            groupBox1.Enabled = true;
+            groupBox2.Enabled = false;
+        }
+
+        private void CargaDatos()
+        {
+            using(DB_PRACTICAEntities db = new DB_PRACTICAEntities())
+            {
+                var lista = from d in db.Personas select d;
+
+                dataGridView1.DataSource = lista.ToList();
+            }
+        }
+
+        private int GetEdad()
+        {
+            int edad = DateTime.Now.Year - fecha_picker.Value.Year;
+
+            if (DateTime.Now.Month < fecha_picker.Value.Month)
+            {
+                edad -= 1;
+            }
+            else if(DateTime.Now.Month == fecha_picker.Value.Month  &&  DateTime.Now.Day < fecha_picker.Value.Day)
+            {
+                edad -= 1;
+            }
+
+            return edad;
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_agregar_Click(object sender, EventArgs e)
+        {
+            vari = "A";
+
+            ModoCarga();
+        }
+
+        private void btn_mod_Click(object sender, EventArgs e)
+        {
+            vari = "M";
+
+            try
+            {
+                Personas people = GetPersona();
+
+                txt_nombre.Text = people.Nombre;
+                fecha_picker.Value = people.Fecha_de_nacimiento;
+                txt_edad.Text = Convert.ToString(people.Edad);
+
+                ModoCarga();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error al intentar pasar los datos: " + ex.Message);
+            }
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            int id = GetId();
+
+            using (DB_PRACTICAEntities db = new DB_PRACTICAEntities())
+            {
+                Personas tabla = db.Personas.Find(id);
+
+                string fecha = tabla.Fecha_de_nacimiento.ToString("dd/MM/yyyy");
+
+                DialogResult respuesta = MessageBox.Show($"Esta seguro que dese eliminar al usuario:\n\nNombre: {tabla.Nombre}\n\nFecha de nacimiento: {fecha}\n\nEdad: {tabla.Edad}", "AVISO", MessageBoxButtons.YesNo);
+
+                if (respuesta == DialogResult.Yes)
+                {
+                    db.Personas.Remove(tabla);
+                    db.SaveChanges();
+                    CargaDatos();
+                }
+
+            }
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (DB_PRACTICAEntities db = new DB_PRACTICAEntities())
+                {
+                    if (vari == "A")
+                    {
+                        int edad = GetEdad();
+
+                        Personas tabla = new Personas();
+
+                        tabla.Nombre = txt_nombre.Text;
+                        tabla.Fecha_de_nacimiento = fecha_picker.Value.Date;
+                        tabla.Edad = edad;
+
+                        db.Personas.Add(tabla);
+
+                    }
+                    else if (vari == "M")
+                    {
+                        int edad = GetEdad();
+
+                        Personas tabla = GetPersona();
+
+                        tabla.Nombre = txt_nombre.Text;
+                        tabla.Fecha_de_nacimiento = fecha_picker.Value.Date;
+                        tabla.Edad = edad;
+
+                        db.Entry(tabla).State = System.Data.Entity.EntityState.Modified;
+                    }
+
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al mostrar: " + ex.Message);
+            }
+
+            CargaDatos();
+            Limpiar();
+            ModoVista();
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            ModoVista();
+            Limpiar();
+        }
+    }
+}
